6656313d17e89714226164be2d9aa3be1face63a
Josh Sarnecki <joshsarnecki@gmail.com>
2022-02-19 02:57:27 +0000

    Started lightcoin testing Classes and extending.  Extended deposit and withdrawl classes to clean up code

--- index.js
+++ index.js
@@ -1,29 +1,83 @@
-let balance = 500.00;
+class Transaction {
 
-class Withdrawal {
-
-  constructor(amount) {
+  constructor(amount, account) {
     this.amount = amount;
-  }
+    this.account = account;
 
-  commit() {
-    balance -= this.amount;
+  }
+}
+
+class Withdrawal extends Transaction {
+
+    commit() {
+    this.account.balance -= this.amount;
   }
 
 }
 
+// class Withdrawal {
+
+//   constructor(amount, account) {
+//     this.amount = amount;
+//     this.account = account;
+//   }
+
+//   commit() {
+//     this.account.balance -= this.amount;
+//   }
+
+// }
+
+class Deposit extends Transaction {
+
+    commit() {
+    this.account.balance += this.amount;
+  }
+}
 
 
+// class Deposit {
 
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
+//   constructor(amount, account) {
+//     this.amount = amount;
+//     this.account = account;
+//   }
 
-t1 = new Withdrawal(50.25);
+//   commit() {
+//     this.account.balance += this.amount;
+//   }
+
+// }
+
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
+
+  }
+
+
+}
+
+
+// ----------------------------------DRIVER CODE BELOW-----------------------------//
+
+let balance = 500.00;
+
+const myAccount = new Account("snow-patrol");
+
+
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log('Balance:',  myAccount.balance);
+
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
